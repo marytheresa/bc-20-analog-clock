6d360a076bd5a5fa042e834b28047f8463eda73b
Mary-Theresa <mtnwankwo@gmail.com>
2017-03-24 19:45:28 +0100

    Add a README.md file

--- js/displayTime.js
+++ js/displayTime.js
@@ -5,9 +5,7 @@ var Clock = function(timezone, canvas) {
 
 Clock.prototype.displayTime = function(val) {
     var date = new Date();
-	var timeDiff = val * 60 + date.getTimezoneOffset();
-	date = new Date(date.getTime() + timeDiff * 60 * 1000);
-	var h = date.getHours();
+	var h = date.getHours() + val;
 	var m = date.getMinutes();
 	var s = date.getSeconds();
     var ctx = this.canvas.getContext("2d");
@@ -15,21 +13,22 @@ Clock.prototype.displayTime = function(val) {
     var x = this.canvas.width / 2;
     var y = this.canvas.height / 2;
   
-    function drawHand(shift, armLength, armColor) {
+    function drawHand(shift, armLength, armColor, armWidth) {
         angle = (2 * Math.PI * shift) - (Math.PI / 2);
         var pointX = x + (Math.cos(angle) * (armLength * radius));
         var pointY = y + (Math.sin(angle) * (armLength * radius));
 
-        ctx.lineWidth = 5;
         ctx.beginPath();
         ctx.moveTo(x, y);
         ctx.lineTo(pointX, pointY);
+        ctx.strokeStyle = armColor
+        ctx.lineWidth = armWidth
         ctx.stroke();
     };
 	ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-	drawHand(h / 12 + m / 60, 0.50, '#000000'); // Hour
-	drawHand(m / 60, 0.75, '#000000'); // Minute
-	drawHand(s / 60, 1.00, '#a32638'); // Second
+	drawHand((h + (m / 60))/ 12, 0.50, '#000000', 10); // Hour
+	drawHand(m / 60, 0.75, '#000000', 5); // Minute
+	drawHand(s / 60, 0.90, '#a32638', 3); // Second
 
     function outerClockFrame() {
         ctx.beginPath();
@@ -49,8 +48,8 @@ Clock.prototype.displayTime = function(val) {
 
             var x1 = x + (Math.cos(angleInRadians) * (radius));
             var y1 = y + (Math.sin(angleInRadians) * (radius));
-            var x2 = x + (Math.cos(angleInRadians) * ((armLength + armLength/3) * radius));
-            var y2 = y + (Math.sin(angleInRadians) * ((armLength + armLength/3) * radius));
+            var x2 = x + (Math.cos(angleInRadians) * ((armLength + armLength/2) * radius));
+            var y2 = y + (Math.sin(angleInRadians) * ((armLength + armLength/2) * radius));
 
             ctx.moveTo(x1, y1);
             ctx.lineTo(x2, y2);
@@ -65,12 +64,24 @@ var clock1 = new Clock("(UTC+01)", document.getElementById("clock1"));
 var clock2 = new Clock("(UTC+01)", document.getElementById("clock2"));
 var clock3 = new Clock("(UTC+01)", document.getElementById("clock3"));
 
+function loadFunction() {
+    // var date = new Date();
+    // console.log(">>>>>>>>>>", date);
+    // console.log(date.getTimezoneOffset() / 60);
+    // console.log(Date.now());
+    setInterval(function() {
+        clock1.displayTime(0);
+    }, 1000)
+}
+
+//document.addEventListener("DOMContentLoaded",  setInterval(clock1.displayTime(1), 1000))//; clock2.displayTime(-12); clock3.displayTime(-12)});
+
 function changeTime() {
+    var date = new Date();
     var e = document.getElementsByClassName("timeZones");
-    console.log(">>>>>>>>>>>>>>>>", e);
     setInterval(function() {
         clock3.displayTime(Number(e[2].value));
         clock2.displayTime(Number(e[1].value));
-        clock1.displayTime(1);
+        clock1.displayTime(0);
     }, 1000)
 }
